663427f463013a5e49817161f42fae5946a0fb31
abarysenka <andrei.barysenka@gmail.com>
2020-07-03 17:20:13 +0200

    Fix " x " pattern

--- prepareGWAS.py
+++ prepareGWAS.py
@@ -29,7 +29,7 @@ for index, row in T[["SNPS","PUBMEDID","CHR_ID","CHR_POS","DISEASE/TRAIT","P-VAL
     b=row["CHR_POS"].split(";")
     c=row["SNPS"].split(";")
     if len(row["CHR_ID"])==0 or len(row["CHR_POS"])==0:
-        if len(c)==1:
+        if len(c)==1 and len(row["SNPS"])!=0:
             m=re.match("^(rs\d+)",row["SNPS"],re.I)
             if m:
                 rs=m.group(1)
@@ -81,7 +81,26 @@ for index, row in T[["SNPS","PUBMEDID","CHR_ID","CHR_POS","DISEASE/TRAIT","P-VAL
                                             print("%s\t%s\t%s\t%s\t%s\t%s" %(c,p,row["SNPS"],row["P-VALUE"],row["DISEASE/TRAIT"],row["PUBMEDID"]))
     else:
         if len(a)==1 and len(b)==1 and len(c)==1:
-            print("%s\t%s\t%s\t%s\t%s\t%s" %(row["CHR_ID"],row["CHR_POS"],row["SNPS"],row["P-VALUE"],row["DISEASE/TRAIT"],row["PUBMEDID"]))
+            m1=re.match("^(\w+)\s+x\s+(\w+)",row["CHR_ID"],re.I)
+            m2=re.match("^(\d+)\s+x\s+(\d+)",row["CHR_POS"],re.I)
+            m3=re.match("^(rs\d+)\s+x\s+(rs\d+)",row["SNPS"],re.I)
+            if not m1 and not m3:
+                print("%s\t%s\t%s\t%s\t%s\t%s" %(row["CHR_ID"],row["CHR_POS"],row["SNPS"],row["P-VALUE"],row["DISEASE/TRAIT"],row["PUBMEDID"]))
+            else:
+                if m2:
+                    print("%s\t%s\t%s\t%s\t%s\t%s" %(m1.group(1),m2.group(1),m3.group(1),row["P-VALUE"],row["DISEASE/TRAIT"],row["PUBMEDID"]))
+                    print("%s\t%s\t%s\t%s\t%s\t%s" %(m1.group(2),m2.group(2),m3.group(2),row["P-VALUE"],row["DISEASE/TRAIT"],row["PUBMEDID"]))
+                else:
+                    rs=m3.group(1)
+                    L=functions.rs2position(rs)
+                    if L:
+                        for z in L:
+                            print("%s\t%s\t%s\t%s\t%s\t%s" %(z["chr"],z["pos"],rs,row["P-VALUE"],row["DISEASE/TRAIT"],row["PUBMEDID"]))
+                    rs=m3.group(2)
+                    L=functions.rs2position(rs)
+                    if L:
+                        for z in L:
+                            print("%s\t%s\t%s\t%s\t%s\t%s" %(z["chr"],z["pos"],rs,row["P-VALUE"],row["DISEASE/TRAIT"],row["PUBMEDID"]))
         elif len(c)!=1:
             for v in c:
                 m=re.match("(rs\d+)",v,re.I)
