5c65e7a7c0c9ea27f670fd4cc976e43a9a4d0f56
abarysenka <andrei.barysenka@gmail.com>
2020-07-03 14:48:31 +0200

    Finalize prepareGWAS.py

--- prepareGWAS.py
+++ prepareGWAS.py
@@ -27,50 +27,67 @@ for index, row in T[["SNPS","PUBMEDID","CHR_ID","CHR_POS","DISEASE/TRAIT","P-VAL
     a=row["CHR_ID"].split(";")
     b=row["CHR_POS"].split(";")
     c=row["SNPS"].split(";")
-    #print("%s\t%s\t%s\t%s\t%s\t%s" %(row["CHR_ID"],row["CHR_POS"],row["SNPS"],row["P-VALUE"],row["DISEASE/TRAIT"],row["PUBMEDID"]))
-    #if len(a)==1 and len(b)==1 and len(c)==1:
-    #    pass
-        #print("%s\t%s\t%s\t%s\t%s\t%s" %(row["CHR_ID"],row["CHR_POS"],row["SNPS"],row["P-VALUE"],row["DISEASE/TRAIT"],row["PUBMEDID"]))
     if len(row["CHR_ID"])==0 or len(row["CHR_POS"])==0:
-        #print(row["SNPS"])
         if len(c)==1:
-            m=re.search("^rs\d+$",row["SNPS"])
+            m=re.match("^(rs\d+)",row["SNPS"],re.I)
             if m:
-                L=functions.rs2position(row["SNPS"])
+                rs=m.group(1)
+                L=functions.rs2position(rs)
                 if L:
                     for z in L:
-                        if z:
-                            print("%s\t%s\t%s\t%s\t%s\t%s" %(z["chr"],z["pos"],row["SNPS"],row["P-VALUE"],row["DISEASE/TRAIT"],row["PUBMEDID"]))
-        
-    # for f in files:
-    #     m=re.search("/([^.]+)\.v8\.egenes.txt.gz",f.name)
-    #     if m:
-    #         tissue=m.group(1)
-    #         infile.extract(f,"/tmp")
-    #         T=pd.read_table("/tmp/"+f.name,compression="gzip",header=0)
-    #         for index, row in T.iterrows():
-    #             id_mapping[row["variant_id"]]=row["rs_id_dbSNP151_GRCh38p7"]
-    #             gene=row["gene_id"]
-    #             m1=re.search("^(ENSG\d+)",gene)
-    #             if m1:
-    #                 gene=m1.group(1)
-    #                 c1=row["gene_chr"]
-    #                 if c1.startswith("chr"):
-    #                     c1=c1[3:]
-    #                 s1=int(row["gene_start"])
-    #                 e1=int(row["gene_end"])
-    #                 if gene in gene_map:
-    #                     c=gene_map[gene]["chr"]
-    #                     s=int(gene_map[gene]["start"])
-    #                     e=int(gene_map[gene]["end"])
-    #                     if c!=c1 or s!=s1 or e!=e1:
-    #                         print("WARNING: in",f.name,": gene",gene,"has multiple coordinates:",sep=" ",file=sys.stderr)
-    #                         print(c1,str(s1),str(e1),sep=" ",file=sys.stderr)
-    #                         print(c,str(s),str(e),sep=" ",file=sys.stderr)
-    #                         print("",file=sys.stderr)
-    #                 else:
-    #                     gene_map[gene]={"chr":c1,"start":s1,"end":e1}
-
-    #         print(tissue,file=sys.stderr)
-    #         print(f.name,file=sys.stderr)
+                        print("%s\t%s\t%s\t%s\t%s\t%s" %(z["chr"],z["pos"],rs,row["P-VALUE"],row["DISEASE/TRAIT"],row["PUBMEDID"]))
+            else:
+                m=re.match("^snp(\d+)-(\d+)",row["SNPS"],re.I)
+                if m:
+                    c=m.group(1)
+                    p=m.group(2)
+                    print("%s\t%s\t%s\t%s\t%s\t%s" %(c,p,row["SNPS"],row["P-VALUE"],row["DISEASE/TRAIT"],row["PUBMEDID"]))
+                else:
+                    m=re.match("^(chr)?(\d+):(\d+)",row["SNPS"],re.I)
+                    if m:
+                        c=m.group(2)
+                        p=m.group(3)
+                        print("%s\t%s\t%s\t%s\t%s\t%s" %(c,p,row["SNPS"],row["P-VALUE"],row["DISEASE/TRAIT"],row["PUBMEDID"]))
+                    else:
+                        m=re.match("^chr:(\d+):(\d+)",row["SNPS"],re.I)
+                        if m:
+                            c=m.group(1)
+                            p=m.group(2)
+                            print("%s\t%s\t%s\t%s\t%s\t%s" %(c,p,row["SNPS"],row["P-VALUE"],row["DISEASE/TRAIT"],row["PUBMEDID"]))
+                        else:
+                            m=re.match("^chr(\d+)[._](\d+)",row["SNPS"],re.I)
+                            if m:
+                                c=m.group(1)
+                                p=m.group(2)
+                                print("%s\t%s\t%s\t%s\t%s\t%s" %(c,p,row["SNPS"],row["P-VALUE"],row["DISEASE/TRAIT"],row["PUBMEDID"]))
+                            else:
+                                m=re.match("^(\d{1,2})-(\d+)",row["SNPS"],re.I)
+                                if m:
+                                    c=m.group(1)
+                                    p=m.group(2)
+                                    print("%s\t%s\t%s\t%s\t%s\t%s" %(c,p,row["SNPS"],row["P-VALUE"],row["DISEASE/TRAIT"],row["PUBMEDID"]))
+                                else:
+                                    m=re.match("^([XY]):(\d+)",row["SNPS"],re.I)
+                                    if m:
+                                        c=m.group(1)
+                                        p=m.group(2)
+                                        print("%s\t%s\t%s\t%s\t%s\t%s" %(c,p,row["SNPS"],row["P-VALUE"],row["DISEASE/TRAIT"],row["PUBMEDID"]))
+                                    else:
+                                        m=re.match("^del-(\d+):(\d+)",row["SNPS"],re.I)
+                                        if m:
+                                            c=m.group(1)
+                                            p=m.group(2)
+                                            print("%s\t%s\t%s\t%s\t%s\t%s" %(c,p,row["SNPS"],row["P-VALUE"],row["DISEASE/TRAIT"],row["PUBMEDID"]))
+    else:
+        if len(a)==1 and len(b)==1 and len(c)==1:
+            print("%s\t%s\t%s\t%s\t%s\t%s" %(row["CHR_ID"],row["CHR_POS"],row["SNPS"],row["P-VALUE"],row["DISEASE/TRAIT"],row["PUBMEDID"]))
+        elif len(c)!=1:
+            for v in c:
+                m=re.match("(rs\d+)",v,re.I)
+                if m:
+                    rs=m.group(1)
+                    L=functions.rs2position(rs)
+                    if L:
+                        for z in L:
+                            print("%s\t%s\t%s\t%s\t%s\t%s" %(z["chr"],z["pos"],rs,row["P-VALUE"],row["DISEASE/TRAIT"],row["PUBMEDID"]))
 
