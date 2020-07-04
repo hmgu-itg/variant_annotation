030cd9a46958d7b5a93fe6a4facbfff93177baad
abarysenka <andrei.barysenka@gmail.com>
2020-07-04 12:53:45 +0200

    Finalize getMousePhenotypes

--- functions.py
+++ functions.py
@@ -891,8 +891,7 @@ def getMouseID(human_ID,build="38"):
     '''Looking up mouse gene ID of a given human gene ID'''
 
     data=restQuery(makeHomologyURL(human_ID,build=build,species="mouse"))
-    #return data
-    print(json.dumps(data,indent=4,sort_keys=True))
+    #print(json.dumps(data,indent=4,sort_keys=True))
 
     mouse_IDs = {}
     if len(data["data"])==0 or len(data["data"][0]["homologies"])==0:
@@ -902,7 +901,7 @@ def getMouseID(human_ID,build="38"):
     for homolog in data["data"][0]["homologies"]:
         try:
             z=restQuery(makeGeneQueryURL(homolog["target"]["id"],build=build))
-            print(json.dumps(z,indent=4,sort_keys=True))
+            #print(json.dumps(z,indent=4,sort_keys=True))
 
             d=""
             m=re.match("(.*)\s+\[.*\]",z["description"])
@@ -918,6 +917,7 @@ def getMgiID(mouse_ID,build="38"):
     '''Looking up MGI cross reference for a given mouse gene ID'''
 
     data=restQuery(makeGeneXQueryURL2(mouse_ID,build=build))
+    #print(json.dumps(data,indent=4,sort_keys=True))
 
     # Now from the returned data we have to pick all possible homolgue IDs:
     for d in data:
@@ -927,7 +927,7 @@ def getMgiID(mouse_ID,build="38"):
             continue
 
     print("[INFO] MGI ID for %s was not found" % (mouse_ID),file=sys.stderr)
-    return None
+    return ""
 
 def getMgiPhenotypes(MGI_ID):
     '''Returns phenotype information stored on http://www.informatics.jax.org/ '''
@@ -939,42 +939,37 @@ def getMgiPhenotypes(MGI_ID):
         df=pd.read_csv(URL, sep="\t")
 
         # Drop unnecessary columns:
-        df.drop([ u'Allele Symbol', u'Chromosome',  u'Synonyms',u'Allele Attributes', u'Transmission', u'Unnamed: 10'], axis=1, inplace=True)
-        df.columns = [u'Allele_ID', u'Allele_name', u'Allele_type',
-           u'Phenotypes',
-           u'Human_disease']
+        df.drop([ "Allele Symbol", "Chromosome",  "Synonyms","Allele Attributes", "Transmission", "Unnamed: 10"], axis=1, inplace=True)
+        df.columns = ["Allele_ID", "Allele_name", "Allele_type","Phenotypes","Human_disease"]
         return df
     except:
-        print("[Info] no phenotype was found!",file=sys.stderr)
-        return None
+        print("[INFO] no phenotype was found for %s" %(MGI_ID),file=sys.stderr)
+        return pd.DataFrame(columns=["Allele_ID", "Allele_name", "Allele_type","Phenotypes","Human_disease"])
 
-# def get_mouse_phenotype (gene_id):
-#     '''returning mouse phenotype given human gene ID'''
-#     # Returning all mouse homologue IDs:
-#     mouse_gene_IDs = _get_mouse_ID(gene_id)
+def getMousePhenotypes(geneID):
+    '''returning mouse phenotype given human gene ID'''
+    # Returning all mouse homologue IDs:
+    mouse_gene_IDs=getMouseID(geneID)
 
-#     # Checking if the returned data is a string, don't bother with the rest:
-#     if isinstance(mouse_gene_IDs, basestring): return mouse_gene_IDs
+    full_dataframe=pd.DataFrame(columns=["Allele_ID", "Allele_name", "Allele_type","Phenotypes","Human_disease","mouse_gene_ID","MGI_ID","mouse_gene_name","mouse_gene_description"])
 
-#     # Looping through all mouse gene IDs, and get the MGI ID:
-#     MGI_IDs = {}
-#     for mouse_gene_ID in mouse_gene_IDs:
-#         MGI_IDs[mouse_gene_ID] = _get_MGI_ID(mouse_gene_ID)
+    if len(mouse_gene_IDs)==0:
+        return full_dataframe
 
-#     # Once we have all the MGI identifiers, we retrieve all the phenotypes:
-#     full_dataframe = ""
-#     for mouse_id, mgi_id in MGI_IDs.iteritems():
-#         df = _get_MGI_phenotypes(mgi_id)
+    MGI_IDs=dict()
+    for mouse_gene_ID in mouse_gene_IDs:
+        MGI_IDs[mouse_gene_ID] = getMgiID(mouse_gene_ID)
 
-#         # Adding extra columns for the record:
-#         df["mouse_gene_ID"] = mouse_id
-#         df["MGI_ID"] = mgi_id
-#         df["mouse_gene_name"] = mouse_gene_IDs[mouse_id][1]
-#         df["mouse_gene_description"] = mouse_gene_IDs[mouse_id][0]
-#         if isinstance(full_dataframe, basestring):
-#             full_dataframe = df
-#         else:
-#             full_dataframe = pd.merge(full_dataframe, df, how="outer")
+    # Once we have all the MGI identifiers, we retrieve all the phenotypes:
+    for mouse_id, mgi_id in MGI_IDs.items():
+        df=getMgiPhenotypes(mgi_id)
 
-#     return full_dataframe
+        # Adding extra columns for the record:
+        df["mouse_gene_ID"] = mouse_id
+        df["MGI_ID"] = mgi_id
+        df["mouse_gene_name"] = mouse_gene_IDs[mouse_id][1]
+        df["mouse_gene_description"] = mouse_gene_IDs[mouse_id][0]
+        full_dataframe = pd.merge(full_dataframe, df, how="outer")
+
+    return full_dataframe
 
