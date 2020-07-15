99bf9c1fc040945ad50b57cbd498444de3a19135
abarysenka <andrei.barysenka@gmail.com>
2020-07-15 14:00:17 +0200

    Add R/makeHeatmap.R

--- test_selenium.py
+++ test_selenium.py
@@ -23,15 +23,15 @@ options.add_argument("--headless")
 
 browser = webdriver.Firefox(firefox_profile=profile,firefox_options=options)
 #browser.get("https://www.ebi.ac.uk/gxa/experiments/E-MTAB-5214/Results?ref=aebrowse")
-browser.get("https://www.ebi.ac.uk/gxa/genes/ENSG00000229183?bs=%7B%22homo%20sapiens%22%3A%5B%22ORGANISM_PART%22%5D%7D&ds=%7B%22kingdom%22%3A%5B%22animals%22%5D%7D#baseline")
+browser.get("https://www.ebi.ac.uk/gxa/genes/ENSG00000169344?bs=%7B%22homo%20sapiens%22%3A%5B%22ORGANISM_PART%22%5D%7D&ds=%7B%22kingdom%22%3A%5B%22animals%22%5D%7D#baseline")
 element=None
 try:
     element = WebDriverWait(browser, 60).until(EC.element_to_be_clickable((By.XPATH, '//button[text()="Download"]')))
 except:
     print("Exception occured",file=sys.stderr)
-    #print(browser.page_source.encode("utf-8"))
 
-#print(HTMLBeautifier.beautify(browser.page_source.encode("utf-8"),4))
+#print(browser.page_source.encode("utf-8"))
+print(HTMLBeautifier.beautify(browser.page_source.encode("utf-8").decode('ascii', 'ignore'),4))
 element.click()
 #print("Element clicked",file=sys.stderr)
 time.sleep(5)
