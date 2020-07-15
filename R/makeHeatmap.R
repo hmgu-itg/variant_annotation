7b02143b6f46394290d5d3914ce91bcef0c01e32
abarysenka <andrei.barysenka@gmail.com>
2020-07-15 15:49:56 +0200

    Add gxa.py

--- R/makeHeatmap.R
+++ R/makeHeatmap.R
@@ -7,5 +7,5 @@ colnames(d1)<-d1[1,]
 d<-d1[-c(1),]
 d1<-as.data.frame(lapply(d[,-c(1)],as.numeric))
 d1[,"Experiment"]<-as.factor(d[,"Experiment"])
-d2<-melt(d1,variable.name=Expression)
+d2<-melt(d1,variable.name="Expression")
 h<-ggplot(data=d2,mapping=aes(x=Experiment,y=Expression,fill=value))+geom_tile()+xlab(label="Experiment")
---
+++ gxa.py
@@ -0,0 +1,93 @@
+import config
+import logging
+import time
+import sys
+from os import rename
+import tempfile
+import pandas as pd
+import re
+
+from selenium.webdriver.firefox.options import Options
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+from query import *
+from utils import *
+
+LOGGER=logging.getLogger(__name__)
+LOGGER.setLevel(logging.DEBUG)
+ch=logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+formatter=logging.Formatter('%(levelname)s - %(name)s - %(asctime)s - %(funcName)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
+ch.setFormatter(formatter)
+LOGGER.addHandler(ch)
+
+# ==============================================================================================================================
+
+def getGxa(ID):
+    '''
+    Geiven a gene ID, get data from GXA (baseline expression)
+
+    Input: gene ID
+    Output: none, data is saved in temporary files (their names saved in "config.GXA_FILES")
+    '''
+
+    fn=saveGxaData(ID)
+    if fn is None:
+        LOGGER.warning("Could not retreive GXA data for %s" % ID)
+        return None
+
+    df=pd.read_table(fn,comment="#",header=0)
+    df=df.set_index("Unnamed: 0")
+    # tissues with highest values
+    tissues=set()
+    idxs=list()
+    for idx in df.index:
+        # only selected experiments
+        if re.search("GTEx",idx.format(),re.I) or re.search("FANTOM",idx.format(),re.I):
+            idxs.append(idx)
+            z=df.loc[idx,:].nlargest(n=int(config.GXA_HIGHEST))
+            tissues.update(set(z.index.format()))
+            
+    df.loc[idxs,list(tissues)].to_csv(config.OUTPUT_DIR+"/"+ID+".tsv",sep="\t")
+
+# ======================================================================================================================
+
+def saveGxaData(ID):
+    """
+    Retreives and saves GXA baseline expression values for a given gene ID
+
+    Input  : gene ID
+    Output : name of the file where the data are stored
+    """
+
+    profile = webdriver.FirefoxProfile()
+    profile.set_preference("browser.download.panel.shown", False)
+    profile.set_preference("browser.helperApps.neverAsk.openFile","text/plain")
+    profile.set_preference("browser.helperApps.neverAsk.saveToDisk", "text/plain")
+    profile.set_preference("browser.download.folderList", 2);
+    profile.set_preference('browser.download.dir', '/tmp')
+    profile.set_preference('browser.download.manager.showWhenStarting', False)
+
+    options = Options()
+    options.add_argument("--headless")
+
+    browser = webdriver.Firefox(firefox_profile=profile,firefox_options=options)
+    browser.get(config.GXA_URL % ID)
+    element=None
+    try:
+        # give it up to 60 seconds to load
+        element = WebDriverWait(browser, 60).until(EC.element_to_be_clickable((By.XPATH, '//button[text()="Download"]')))
+    except:
+        LOGGER.error("Exception occured")
+        return None
+
+    element.click()
+    time.sleep(5)
+    browser.quit()
+    tfn=tempfile.TemporaryFile().name
+    rename("/tmp/expression_atlas-homo_sapiens.tsv",tfn)
+    return tfn
