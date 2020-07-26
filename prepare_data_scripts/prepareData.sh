6b262e607bd7cba533c603a5759ab1a6dd8bee55
abarysenka <andrei.barysenka@gmail.com>
2020-07-26 18:49:59 +0200

    Update prepareData.sh

--- prepare_data_scripts/prepareData.sh
+++ prepare_data_scripts/prepareData.sh
@@ -2,15 +2,21 @@
 
 function usage {
     echo ""
-    echo "Usage: $0 -o <output dir>"
-#    echo "Usage: $0 -x <input GTEx.tar>"
-#    echo "          -o <output dir>"
+    echo "Usage: $0 -o <output dir> { -g -x -r }"
+    echo " -g -x -r are optional, for selecting which data to prepare  (GWAS, GTEx or Regulation)"
 }
 
 OPTIND=1
-while getopts "o:" optname; do
+
+prepgwas=0
+prepgtex=0
+prepreg=0
+
+while getopts "gxro:" optname; do
     case "$optname" in
-#        "x" ) gtex="${OPTARG}";;
+        "g" ) prepgwas=1;;
+        "x" ) prepgtex=1;;
+        "r" ) prepreg=1;;
         "o" ) out="${OPTARG}";;
         "?" ) usage ;;
         *) usage ;;
@@ -22,10 +28,11 @@ if [[ $# -eq 0 ]];then
     exit 0
 fi
 
-# if [[ ! -f "$gtex" ]];then
-#     echo $(date '+%d/%m/%Y %H:%M:%S') "ERROR: GTEx file ($gtex) does not exist"
-#     exit 1
-# fi
+if [[ "$prepgwas" -eq 0 && "$prepgtex" -eq 0 && "$prepreg" -eq 0 ]];then
+    prepgwas=1
+    prepgtex=1
+    prepreg=1
+fi
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 CDIR=$(pwd)
@@ -36,6 +43,14 @@ gtex_script="$DIR"/"prepareGTEx.py"
 
 regdir=${regdir/%\/}
 out=${out/%\/}
+
+# ------------------------------- URLs ------------------------------------
+
+gtexURL="https://storage.googleapis.com/gtex_analysis_v8/single_tissue_qtl_data/GTEx_Analysis_v8_eQTL.tar"
+gtexlookupURL="https://storage.googleapis.com/gtex_analysis_v8/reference/GTEx_Analysis_2017-06-05_v8_WholeGenomeSeq_838Indiv_Analysis_Freeze.lookup_table.txt.gz"
+gtexgencodeURL="https://storage.googleapis.com/gtex_analysis_v8/reference/gencode.v26.GRCh38.genes.gtf"
+gwasURL="https://www.ebi.ac.uk/gwas/api/search/downloads/alternative"
+ensregURL="ftp://ftp.ensembl.org/pub/release-100/regulation/homo_sapiens/RegulatoryFeatureActivity/"
 
 # -------------------------------------------------------------------------
 
@@ -48,22 +63,22 @@ mkdir -p "$out/temp"
 # ----------------------- DOWNLOADING DATA --------------------------------
 
 echo $(date '+%d/%m/%Y %H:%M:%S') "Downloading GTEx data"
-wget --quiet -c "https://storage.googleapis.com/gtex_analysis_v8/single_tissue_qtl_data/GTEx_Analysis_v8_eQTL.tar" -O "$out/temp/GTEx.tar"
+wget --quiet -c "$gtexURL" -O "$out/temp/GTEx.tar"
 
 echo $(date '+%d/%m/%Y %H:%M:%S') "Downloading GTEx lookup data"
-wget --quiet -c "https://storage.googleapis.com/gtex_analysis_v8/reference/GTEx_Analysis_2017-06-05_v8_WholeGenomeSeq_838Indiv_Analysis_Freeze.lookup_table.txt.gz" -O "$out/gtex/GTEx.lookup.txt.gz"
+wget --quiet -c "$gtexlookupURL" -O "$out/gtex/GTEx.lookup.txt.gz"
 
 echo $(date '+%d/%m/%Y %H:%M:%S') "Downloading GTEx Gencode data"
-wget --quiet -c "https://storage.googleapis.com/gtex_analysis_v8/reference/gencode.v26.GRCh38.genes.gtf" -O "$out/gtex/gencode.gtf"
+wget --quiet -c "$gtexgencodeURL" -O "$out/gtex/gencode.gtf"
 
 echo $(date '+%d/%m/%Y %H:%M:%S') "Downloading GWAS catalog"
-wget --quiet -c "https://www.ebi.ac.uk/gwas/api/search/downloads/alternative" -O "$out/gwas/gwas_full.tsv"
+wget --quiet -c "$gwasURL" -O "$out/gwas/gwas_full.tsv"
 
 cd "$out/temp"
 mkdir -p regulation
 cd regulation
 echo $(date '+%d/%m/%Y %H:%M:%S') "Downloading Ensembl Regulation"
-lftp -c 'open ftp://ftp.ensembl.org/pub/release-100/regulation/homo_sapiens/RegulatoryFeatureActivity/; mirror -P . .' > /dev/null 2> /dev/null
+lftp -c "open $ensregURL; mirror -P . ." > /dev/null 2> /dev/null
 
 cd "$CDIR"
 
