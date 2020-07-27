ef87639befd890bedc1dbdc8dc48543069cc750a
abarysenka <andrei.barysenka@gmail.com>
2020-07-28 01:53:21 +0200

    Fix variant.py

--- modules/variant.py
+++ modules/variant.py
@@ -304,7 +304,7 @@ def id2rs(varid,build="38"):
                 spdis=x["spdi"]
                 var=x["id"][0]
                 for spdi in spdis:
-                    h=parseSPDI(spdi,alleles=True)
+                    h=query.parseSPDI(spdi,alleles=True)
                     ref=h["ref"]
                     alt=h["alt"]
                     p=h["pos"]
