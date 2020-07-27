b7c404fda14a388c44f0d80abde058b2b2381350
abarysenka <andrei.barysenka@gmail.com>
2020-07-28 00:14:41 +0200

    Minor fixes

--- test_scripts/test_id2rs.py
+++ test_scripts/test_id2rs.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
 
-import sys, time
+import sys
 import os
 import argparse
 import re
-import datetime
-from variant import *
+
+from modules import variant
+
 #----------------------------------------------------------------------------------------------------------------------------------
 
 build="38"
-window=0
 
 parser = argparse.ArgumentParser(description="Get rs ID for given variant ID")
 parser.add_argument('--build','-b', action="store",help="Genome build: default: 38", default="38")
-parser.add_argument('--window','-w', action="store",help="bp window around variant: default: 0", default="0")
 requiredArgs=parser.add_argument_group('required arguments')
 requiredArgs.add_argument('--id','-i', action="store",help="varID",required=True)
 
@@ -30,9 +29,6 @@ except:
 if args.build!=None:
     build=args.build
 
-if args.window:
-    window=int(args.window)
-
 varID=args.id
 m=re.search("^(\d+)_(\d+)_([ATGC]+)_([ATGC]+)",varID)
 chrom=m.group(1)
@@ -41,17 +37,7 @@ a1=m.group(3)
 a2=m.group(4)
 
 #---------------------------------------------------------------------------------------------------------------------------
-#r=restQuery(makeOverlapVarQueryURL(chrom,pos-window,pos+window,build))
-#print(json.dumps(r, indent=4, sort_keys=True))
 
-for x in id2rs(varID,build):
+for x in variant.id2rs(varID,build):
     print(x,varID,sep="\t",file=sys.stdout)
-
-# for x in stringdiff("AAAGAAAAGAAA","AAA"):
-#     print(x)
-
-# print("")
-# for x in stringdiff("AGCCA","AGCCAGCCA"):
-#     print(x)
-
 
