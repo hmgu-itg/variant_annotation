5bfef9600b6d4e374aaf6fa33e6e17f254749092
abarysenka <andrei.barysenka@gmail.com>
2020-07-27 10:00:42 +0200

    Remove functions.py

--- modules/variant.py
+++ modules/variant.py
@@ -490,3 +490,90 @@ def population2df(pop_data):
         i+=1
 
     return df
+
+# ==============================================================================================================================
+
+# Retrieve data from the OMIM database
+# def getOmimInfo (cross_refs):
+#     '''
+#     Required information:
+#         - list of OMIM ID-s
+
+#     Retrieved information:
+#         - OMIM entry name
+#         - text field name
+#         - text field (that includes references as well)
+
+#     Retrieved data structure (one dict for all queried entries):
+#     dict{
+#         OMIM_ID :{
+#             title : OMIM_preferredTitle
+#             text : {
+#                 OMIM_textSectionTitle : OMIM_textSectionContent
+#     }}}
+
+#     More information how the OMIM API works see: http://omim.org/help/api
+#     The API key will expire in 2016.11.11, at that time a new key have to be required.
+#     '''
+
+#     # Extracting OMIM ID from the cross refereces data:
+#     MIM_IDs = [x[1] for x in cross_refs["MIM disease"]]
+#     MIM_IDs.extend([x[1] for x in cross_refs["MIM gene"]])
+
+#     # The function returns at this point if there is not MIM IDs in the crossref
+#     if len(MIM_IDs) == 0:
+#         return "No OMIM entry for this gene."
+
+#     # Constructing query string:
+#     URL = config.OMIM_URL
+
+#     for ID in MIM_IDs:
+#         URL += 'mimNumber=%s&' % ID
+
+#     # Retrieving the following fields:
+#     URL += 'include=text&'
+#     URL += 'include=allelicVariantList&'
+#     URL += 'include=referenceList&'
+
+#     # Adding API key:
+#     URL += config.OMIM_APIKEY
+
+#     # Required format is pyton (although it is a string indeed)ormat=python
+#     URL += "format=python&"
+
+#     # Downloading page:
+#     page = requests.get(URL)
+
+#     # Reconstructingh dictionary from the returned string:
+#     OMIM_entry  = ast.literal_eval(page.content)
+
+#     # hash to fill in:
+#     OMIM_data = {}
+
+#     # Parsing hash:
+#     for entry in OMIM_entry['omim']['entryList']:
+#         # GEt OMIM ID:
+#         ID = entry["entry"]["mimNumber"]
+#         OMIM_data[ID] = {}
+
+#         # Get OMIM name:
+#         OMIM_data[ID]['title'] = entry["entry"]["titles"]['preferredTitle']
+
+#         # Get OMIM text:
+#         OMIM_data[ID]['text'] = {}
+#         for fields in entry['entry']["textSectionList"]:
+#             OMIM_data[ID]['text'][fields['textSection']['textSectionTitle']] = fields['textSection']['textSectionContent']
+
+#         # now we have to parse allelic variants:
+#         # print stuff['omim']['entryList'][0]['entry']['allelicVariantList'][0]['allelicVariant'].keys()
+#         # ['status', 'name', 'dbSnps', 'text', 'mutations', 'number', 'alternativeNames', 'clinvarAccessions']
+
+#         try:
+#             OMIM_data[ID]['variations'] = {}
+#             for variations in entry['entry']['allelicVariantList']:
+#                 OMIM_data[ID]['variations'][variations['allelicVariant']['dbSnps']] = variations['allelicVariant']['text']
+#         except:
+#             continue
+
+#     return OMIM_data
+
