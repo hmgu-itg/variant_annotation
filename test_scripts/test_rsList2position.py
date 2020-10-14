d2023e445f457e578e0c623b87af1c14cf749107
andrei <andrei.barysenka@helmholtz-muenchen.de>
2020-10-14 13:50:39 +0000

    Finalize test_rsList2position.py

--- test_scripts/test_rsList2position.py
+++ test_scripts/test_rsList2position.py
@@ -1,33 +1,13 @@
 #!/usr/bin/env python3
 
-import argparse
-import sys
+import json
 
-from modules import variant
-from modules import gene
+from python.varannot import variant
 
-sys.stdout=open(sys.stdout.fileno(),mode='w',encoding='utf8',buffering=1)
+L=["rs1261047350","rs777575161","rs1250100640"]
 
-# ------------------------------------------------------------------------------------------------------------------------
+x=variant.rs2position("rs1261047350",build="38")
+print(json.dumps(x,indent=4,sort_keys=True))
 
-build="38"
-
-parser = argparse.ArgumentParser()
-input_options = parser.add_argument_group('Input options')
-input_options.add_argument("--id", "-i", help="Required: input variation, rsID",required=True)
-
-args=parser.parse_args()
-VAR_ID=args.id
-
-variant_data=variant.getVariantInfo(VAR_ID,build)
-if variant_data is None:
-    sys.exit(1)
-chrpos=variant.getChrPosList(variant_data["mappings"])
-
-all_genes=list()
-for i in range(0,len(chrpos)):
-    gene_list=gene.getGeneList(chrpos[i][0],chrpos[i][1],build=build)
-    if gene_list:
-        all_genes.extend(gene_list)
-
-print("%s\t%d" %(VAR_ID,len(all_genes)))
+x=variant.rsList2position(L,build="38")
+print(json.dumps(x,indent=4,sort_keys=True))
