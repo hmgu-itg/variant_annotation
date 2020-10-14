14deb20c04c8fd0c8b6a3814516c7ce24a251121
andrei <andrei.barysenka@helmholtz-muenchen.de>
2020-10-14 15:24:44 +0000

    Update rs2position.py

--- rs2position.py
+++ rs2position.py
@@ -7,14 +7,14 @@ import re
 import logging
 import json
 
-from modules import variant
-from modules import query
+from python.varannot import variant
+from python.varannot import query
 
 #----------------------------------------------------------------------------------------------------------------------------------
 
 build="38"
 
-parser = argparse.ArgumentParser(description="Get rs ID for given variant ID")
+parser = argparse.ArgumentParser(description="Get chr:pos:ref:alt for a given rs")
 parser.add_argument('--build','-b', action="store",help="Genome build: default: 38", default="38")
 requiredArgs=parser.add_argument_group('required arguments')
 requiredArgs.add_argument('--id','-i', action="store",help="rsID",required=True)
@@ -42,12 +42,10 @@ formatter=logging.Formatter('%(levelname)s - %(name)s - %(asctime)s - %(message)
 ch.setFormatter(formatter)
 LOGGER.addHandler(ch)
 
-logging.getLogger("modules.variant").addHandler(ch)
-logging.getLogger("modules.variant").setLevel(logging.DEBUG)
+logging.getLogger("python.varannot.variant").addHandler(ch)
+logging.getLogger("python.varannot.variant").setLevel(logging.DEBUG)
 
 #---------------------------------------------------------------------------------------------------------------------------
 
-for x in variant.rs2position(varID,build):
-    print(varID,x["chr"],x["pos"],sep="\t",file=sys.stdout)
-    #r=query.restQuery(query.makeOverlapVarQueryURL(x["chr"],x["pos"],x["pos"],build=build))
-    #print(json.dumps(r,indent=4,sort_keys=True))
+for x in variant.rs2position(varID,build,alleles=True):
+    print(varID,x["chr"],x["pos"],x["ref"],x["alt"],sep="\t",file=sys.stdout)
