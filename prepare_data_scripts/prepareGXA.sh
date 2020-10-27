29e5a1c15cdcaf7233c9588a8e4fda595e372f6d
andrei <andrei.barysenka@helmholtz-muenchen.de>
2020-10-27 17:25:40 +0000

    Update prepareGXA.sh

--- prepare_data_scripts/prepareGXA.sh
+++ prepare_data_scripts/prepareGXA.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-getBaselineExperiments.py | sed 's/ class="[^"]*"//g' | sed 's/<\/div><\/div>/\n/g'|sed 's/<div><div><span>/\n<div><div><span>/g' |perl -lne 'print $1 if /^\<div\>\<div\>\<span\>(.*)\<\/div\>\<\/label\>$/;'
-
+getBaselineExperiments.py | sed 's/ class="[^"]*"//g' | sed 's/<\/div><\/div>/\n/g'|sed 's/<div><div><span>/\n<div><div><span>/g' |perl -lne 'print $1 if /^\<div\>\<div\>\<span\>(.*)\<\/div\>\<\/label\>$/;' |perl -lne 'print $1."\t".$2."\t".$3 if /.*\/experiments\/(.*)?\/Results.*Design\"\>\<ul\>(.*)?\<\/ul\>\<\/a\>.*\<div\>\<span\>\<ul\>(.*)?\<\/ul\>\<\/span\>\<\/div\>/;' | sed 's/<\/li><li>/;/g' | sed 's/<li>//g' | sed 's/<\/li>//g' | grep "organism part" | grep -v disease | grep -v "cell line" 
