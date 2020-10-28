663407f58a6ba0c4f818104fadc5f7f3428045e9
andrei <andrei.barysenka@helmholtz-muenchen.de>
2020-10-28 11:32:49 +0000

    Update concat.py

--- concat.py
+++ concat.py
@@ -7,10 +7,18 @@ import os
 if len(sys.argv[1:])==0:
     sys.exit(0)
 
+#pd.set_option("display.max_rows", None, "display.max_columns", None)
+
+dfs=list()
 for i in range(1,len(sys.argv)):
-    print(sys.argv[i])
+    print(sys.argv[i],file=sys.stderr)
     bname=os.path.basename(sys.argv[i])
     name=bname[:-4]
-    DF=pd.read_table(sys.argv[i],header=None,comment="#")
-
+    df=pd.read_table(sys.argv[i],header=0,comment="#",low_memory=False)
+    df.set_index(keys="Gene ID",inplace=True,verify_integrity=True)
+    df["Study"]=name
+    dfs.append(df)
+    
+DF=pd.concat(dfs)
+DF.to_csv(path_or_buf=sys.stdout,sep="\t")
 
