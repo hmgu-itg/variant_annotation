aea44e7701aa74e0d5674a93aee320daab3141f9
andrei <andrei.barysenka@helmholtz-muenchen.de>
2021-04-09 02:41:31 +0200

    Add argument parsing

--- selectPeaks.py
+++ selectPeaks.py
@@ -1,9 +1,39 @@
 #!/usr/bin/python3
 
 import sys
+import argparse
 import pandas as pd
 
-def find_peaks(df,flank):
+#----------------------------------------------------------------------------------------------------------------------------------
+
+parser = argparse.ArgumentParser(description="Select signal peaks from association results")
+parser.add_argument('--input','-i',type=str,action="store",help="Input file with association results",required=True)
+parser.add_argument('--threshold','-t',type=float,action="store",help="P-value threshold",required=True)
+parser.add_argument('--flank','-f',type=int,action="store",help="flank size (bp)",required=True)
+parser.add_argument('--chr','-c',type=str,action="store",help="Chromosome label column",required=True)
+parser.add_argument('--pos','-p',type=str,action="store",help="Variant position column",required=True)
+parser.add_argument('--pval','-v',type=str,action="store",help="P-value column",required=True)
+
+if len(sys.argv[1:])==0:
+    parser.print_help()
+    sys.exit(0)
+
+try:
+    args=parser.parse_args()
+except:
+    parser.print_help()
+    sys.exit(0)
+
+infile=args.input
+pt=args.threshold
+flank=args.flank
+chrname=args.chr
+posname=args.pos
+pvalname=args.pval
+
+#----------------------------------------------------------------------------------------------------------------------------------
+
+def find_peaks(df,flank,posname,pvalname):
     # df is position sorted
     L=[]
     peak_p=1.0
@@ -11,67 +41,34 @@ def find_peaks(df,flank):
 
     for idx,row in df.iterrows():
         if peak is None:
-            peak=row["ps"]
-            peak_p=row["P-value"]
+            peak=row[posname]
+            peak_p=row[pvalname]
         else:
-            if peak+flank<row["ps"]:
+            if peak+flank<row[posname]:
                 L.append(peak)
-                peak=row["ps"]
-                peak_p=row["P-value"]
+                peak=row[posname]
+                peak_p=row[pvalname]
             else:
-                if row["P-value"]<peak_p:
-                    peak=row["ps"]
-                    peak_p=row["P-value"]
+                if row[pvalname]<peak_p:
+                    peak=row[posname]
+                    peak_p=row[pvalname]
     L.append(peak)
-                    
-    criterion=df["ps"].map(lambda x: x in L)
+    criterion=df[posname].map(lambda x: x in L)
     ret=df.loc[criterion].copy()
     return ret
 
-fname=sys.argv[1]
-
 chunks=[]
-pt=0.001
-#print("Input: %s" %(fname))
-for c in pd.read_table(fname,sep="\t",iterator=True,chunksize=100,compression="gzip"):
-    #t=c.loc[lambda c: c["P-value"].astype(float)<0.01]
-    t=c.loc[pd.to_numeric(c["P-value"],errors="coerce").notnull()]
-    t=t.loc[t["P-value"].astype(float)<pt]
+for c in pd.read_csv(fname,sep="\t",iterator=True,chunksize=10000):
+    t=c.loc[pd.to_numeric(c[pvalname],errors="coerce").notnull()]
+    t=t.loc[t[pvalname].astype(float)<pt]
     chunks.append(t)
     
 df=pd.concat(chunks)
-df=df.astype({"P-value":float,"ps":int})    
-#print(df.dtypes)    
+df=df.astype({pvalname:float,posname:int})    
 
-#print(df.columns)
-#if "P-value" in df.columns:
-#    print("\"P-value\" is present")
-
-#print(df[["P-value"]].head())
-#print(df.loc[range(100,120)])
-nrows=df.shape[0]
-ncols=df.shape[1]
-#print("Dim: %d x %d" % (nrows,ncols))
-
-flank=50000
-for c,tab in df.groupby("#chr"):
-    #print(tab.sort_values(by="ps"),end="\n\n")
-    r=find_peaks(tab.sort_values(by="ps"),flank)
-    #r.reset_index(inplace=True)
-    #r["label"]=str(c)+"_"+r.index
-    #r["label"]="chr"+str(c)+"_peak"+r.index.map(str)
-    #r["label"]=str(c)+"_"+r["label"]
-    #r.set_index("label",inplace=True)
-    #r.drop("index",axis=1,inplace=True)
+for c,tab in df.groupby(chrname):
+    r=find_peaks(tab.sort_values(by=posname),flank,posname,pvalname)
     print(r.to_string())
     r.to_csv("peaks_chr"+str(c)+".txt",sep="\t",index=False)
-    #print(r.dtypes)    
-    #print("------------------------------------------------\n\n")
-    
-#print(df.sort_values(by="P-value"))
-#print(df.groupby(by="#chr"))
 
-# for i in range(len(df.sort_values(by="P-value"))):
-#     print(df.loc[i,"#chr"], df.loc[i,"ps"])
-    
-exit(0)
+sys.exit(0)
