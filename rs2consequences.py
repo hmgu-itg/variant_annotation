29677d261a395ed50492f664f8825ad3c4b09f2c
git <andrei.barysenka@helmholtz-muenchen.de>
2021-07-27 08:32:02 +0000

    Finalize rs2consequences.py

--- rs2consequences.py
+++ rs2consequences.py
@@ -8,15 +8,15 @@ import logging
 
 from varannot import utils
 from varannot import query
-from varannot import config
 
 #----------------------------------------------------------------------------------------------------------------------------------
 
 build="38"
 verbosity=logging.INFO
 
-parser = argparse.ArgumentParser(description="Get VEP consequences for variant IDs (read from STDIN)")
+parser = argparse.ArgumentParser(description="Get VEP consequences for rs ID")
 parser.add_argument('--build','-b', action="store",help="Genome build: default: 38", default="38",required=False)
+parser.add_argument('--id','-i', action="store",help="rs ID",required=True)
 parser.add_argument("--verbose", "-v", help="Optional: verbosity level", required=False,choices=("debug","info","warning","error"),default="info")
 
 try:
@@ -36,7 +36,9 @@ if args.verbose is not None:
     elif args.verbose=="error":
         verbosity=logging.ERROR
 
-LOGGER=logging.getLogger("id2consequences")
+rsID=args.id
+
+LOGGER=logging.getLogger("rs2consequences")
 LOGGER.setLevel(verbosity)
 ch=logging.StreamHandler()
 ch.setLevel(verbosity)
@@ -47,37 +49,18 @@ LOGGER.addHandler(ch)
 logging.getLogger("varannot.utils").addHandler(ch)
 logging.getLogger("varannot.utils").setLevel(verbosity)
 
-def f(ID):
-    L=ID.split("_")
-    L.insert(2,ID)
-    return " ".join(L)+" . . ."
-
 #---------------------------------------------------------------------------------------------------------------------------
 
-for L in utils.chunks([line.rstrip() for line in sys.stdin.readlines()],config.VEP_POST_MAX):
-    string="{\"variants\":[\""+"\",\"".join(list(map(lambda x:f(x),L)))+"\"]}"
-    LOGGER.debug("data: %s" %(string))
-    r=query.restQuery(query.makeVepListQueryURL(build=build),data=string,qtype="post")
-    if r:
-        print(json.dumps(r,indent=4,sort_keys=True))
-        for x in r:
-            rsid="NA"
-            if "colocated_variants" in x:
-                if "id" in x["colocated_variants"][0]:
-                    rsid=x["colocated_variants"][0]["id"]
-            mcsq=x["most_severe_consequence"] if "most_severe_consequence" in x else "NA";
-            H={}
-            if "transcript_consequences" in x:
-                for g in x["transcript_consequences"]:
-                    gene_id=g["gene_id"]
-                    csq=g["consequence_terms"][0]
-                    if gene_id in H:
-                        H[gene_id].append(csq)
-                    else:
-                        H[gene_id]=[csq]
-                    LOGGER.debug("%s\t%s\t%s\t%s" %(x["id"],rsid,gene_id,csq))
-                for g in H:
-                    print("%s\t%s\t%s\t%s" %(x["id"],rsid,g,utils.getMostSevereConsequence(H[g])))
-            else:
-                print("%s\t%s\t%s\t%s" %(x["id"],rsid,"NA",mcsq))
-
+msqs=list()
+g2c=dict()
+r=query.restQuery(query.makeVepRSQueryURL(rsID,build=build))
+if r:
+    for x in r:
+        msqs.append(x["most_severe_consequence"])
+        for t in x["transcript_consequences"]:
+            g2c.setdefault(t["gene_symbol"],[]).extend(t["consequence_terms"])
+msq=utils.getMostSevereConsequence(msqs)
+for g in g2c:
+    if msq in g2c[g]:
+        print("%s\t%s\t%s" %(rsID,g,msq))
+        break
