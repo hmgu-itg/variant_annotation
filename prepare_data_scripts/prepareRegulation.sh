8181dcf44107c683a6f87bf2c704dfbb184fcbe7
andrei-barysenka <andrei.barysenka@gmail.com>
2022-04-01 14:22:20 +0000

    add more logging

--- prepare_data_scripts/prepareRegulation.sh
+++ prepare_data_scripts/prepareRegulation.sh
@@ -22,31 +22,30 @@ if [[ $# -eq 0 ]];then
     exit 0
 fi
 
-#tmpfile=$(mktemp "$PWD"/tmp_regulation.XXXX)
-
 for f in $(find "$indir" -name "*.gff.gz");do 
     b=$(basename $f)
-    #echo "Current file: $b"
     cell_type=$(echo $b| perl -lne '$x="NA";if (/homo_sapiens\.GRCh38\.(\w+)\.Regulatory_Build\.regulatory_activity\.\d+\.gff\.gz/){$x=$1;} print $x;')
-    #echo "Cell type: $cell_type"
+    echo $(date '+%d/%m/%Y %H:%M:%S') "Regulation: current file: $b; current cell type: $cell_type"
     if [[ "$cell_type" != "NA" ]];then
 	export cell_type="$cell_type"
 	zcat "$f" | perl -F"\t" -lane '$ct=$ENV{cell_type};$,="\t";$a="NA";$id="NA";if ($F[8] =~ /activity=([^;]+);/){$a=$1;}if ($F[8] =~ /regulatory_feature_stable_id=([^;]+)/){$id=$1;} print $F[0],$F[3]-1,$F[4],$F[2],$ct,$a,$id;' >> "$out"
     else	
-	echo "WARNING: file $f has wrong name pattern"
-	echo "Expected pattern: homo_sapiens.GRCh38.\"cell_type_name\".Regulatory_Build.regulatory_activity.\\d+.gff.gz"
-	echo "Skipping"
+	echo $(date '+%d/%m/%Y %H:%M:%S') "WARNING: file $f has wrong name pattern"
+	echo $(date '+%d/%m/%Y %H:%M:%S') "Expected pattern: homo_sapiens.GRCh38.\"cell_type_name\".Regulatory_Build.regulatory_activity.\\d+.gff.gz"
+	echo $(date '+%d/%m/%Y %H:%M:%S') "Skipping"
 	echo ""
 	continue
     fi
 done
 
-echo "Sorting"
+echo $(date '+%d/%m/%Y %H:%M:%S') "Regulation: sorting"
 sort -k1,1 -k2,2n "$out" | sponge "$out"
-echo "Compressing"
+echo $(date '+%d/%m/%Y %H:%M:%S') "Regulation: compressing"
 bgzip -f "$out"
-echo "Indexing"
+echo $(date '+%d/%m/%Y %H:%M:%S') "Regulation: indexing"
 tabix -p bed "$out".gz
-#rm "$tmpfile"
-echo "Done"
+echo $(date '+%d/%m/%Y %H:%M:%S') "Regulation done"
+echo ""
+
+exit 0
 
