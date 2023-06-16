ea3c73fa40d31a88906699212c53bf21b2a2fb07
andrei <andrei@linux.fritz.box>
2023-06-16 13:54:40 +0000

    add piping output to json2table

--- genereport/cmdkp_top_associations_region.sh
+++ genereport/cmdkp_top_associations_region.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-curl -s -X 'GET' 'https://bioindex.hugeamp.org/api/bio/query/top-associations?q='$(echo $1|sed "s/:/%3A/"|sed "s/,//g")'&fmt=row'   -H 'accept: application/json' -o $2
+if [[ $# -lt 1 ]]; then
+    echo "Arguments: chr:start-end {output file; default: STDOUT}"
+    exit 0
+fi
+
+if [[ $# -gt 1 ]]; then
+    curl -s -X 'GET' 'https://bioindex.hugeamp.org/api/bio/query/top-associations?q='$(echo $1|sed "s/:/%3A/"|sed "s/,//g")'&fmt=row' -H 'accept: application/json' |./json2table.py > $2
+else
+    curl -s -X 'GET' 'https://bioindex.hugeamp.org/api/bio/query/top-associations?q='$(echo $1|sed "s/:/%3A/"|sed "s/,//g")'&fmt=row' -H 'accept: application/json'|./json2table.py
+fi
