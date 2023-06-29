707ab3d945a06754a86a818ed8e3fd26c1f24500
andrei <andrei@linux.fritz.box>
2023-06-29 19:52:41 +0000

    add gene2info

---
+++ gene2info.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+
+import sys
+import os
+import argparse
+import logging
+import json
+import pandas as pd
+
+from varannot import gene
+
+#----------------------------------------------------------------------------------------------------------------------------------
+
+def main():
+    verbosity=logging.INFO
+    parser = argparse.ArgumentParser(description="General gene info")
+    parser.add_argument("--verbose", "-v", help="Optional: verbosity level", required=False,choices=("debug","info","warning","error"),default="info")
+    parser.add_argument('--id','-i', action="store",help="Gene ID",required=False,default=None)
+    parser.add_argument('--build','-b', action="store",help="Genome build: default: 38",required=False,default="38")
+
+    try:
+        args=parser.parse_args()
+    except:
+        sys.exit(0)
+
+    if args.verbose is not None:
+        if args.verbose=="debug":
+            verbosity=logging.DEBUG
+        elif args.verbose=="warning":
+            verbosity=logging.WARNING
+        elif args.verbose=="error":
+            verbosity=logging.ERROR
+
+    build=args.build
+    ID=args.id
+
+    LOGGER=logging.getLogger("gene2info")
+    LOGGER.setLevel(verbosity)
+    ch=logging.StreamHandler()
+    ch.setLevel(verbosity)
+    formatter=logging.Formatter('%(levelname)s - %(name)s - %(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
+    ch.setFormatter(formatter)
+    LOGGER.addHandler(ch)
+
+    logging.getLogger("varannot.gene").addHandler(ch)
+    logging.getLogger("varannot.gene").setLevel(verbosity)
+
+    if sys.stdin.isatty() and ID is None:
+        parser.print_help()
+        sys.exit(1)
+        
+    #---------------------------------------------------------------------------------------------------------------------------
+
+    data=gene.getGeneInfo(ID,build=build)
+    if data is None:
+        sys.exit(1)
+    df=pd.json_normalize([data])
+    df.to_csv(sys.stdout,index=False,sep="\t",na_rep="NA",columns=["id","name","chromosome","start","end","description","type","strand"])
+
+if __name__=="__main__":
+    main()
