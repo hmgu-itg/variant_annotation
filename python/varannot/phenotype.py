8fa1f0176e255d12872784e447c5430d3e4d9ea9
andrei <andrei@linux.fritz.box>
2023-07-01 21:26:20 +0000

    add byRegion

--- python/varannot/phenotype.py
+++ python/varannot/phenotype.py
@@ -11,13 +11,20 @@ LOGGER=logging.getLogger(__name__)
 def byGene(gene,build="38"):
     '''
     Return phenotype annotations for a given gene
-
-    Input: gene name, build
-    Output: pandas data frame with columns rs,Location,Phenotype,P-value,Link
     '''
     
     # df=pd.DataFrame(columns=["rs","Location","Phenotype","P-value","Link"])
     r=query.restQuery(query.makeGenePhenotypeQueryURL(gene,build=build,pubmed=False))
+    if r:
+        LOGGER.debug("%s" % json.dumps(r,indent=4,sort_keys=True))
+    return r
+
+def byRegion(chrom,start,end,build="38"):
+    '''
+    Return phenotype annotations for a given region
+    '''
+    
+    r=query.restQuery(query.makeOverlapPhenotypeQueryURL(chrom,start,end,build=build))
     if r:
         LOGGER.debug("%s" % json.dumps(r,indent=4,sort_keys=True))
     return r
