8fb211e2bc811df2d4dae8f2ddfb08fb79452ba1
andrei <andrei@linux.fritz.box>
2023-07-10 21:59:40 +0000

    add json support

--- gene2uniprot.py
+++ gene2uniprot.py
@@ -17,7 +17,9 @@ def main():
     parser = argparse.ArgumentParser(description="Uniprot information for a gene")
     parser.add_argument("--verbose", "-v", help="Optional: verbosity level", required=False,choices=("debug","info","warning","error"),default="info")
     parser.add_argument('--id','-i', action="store",help="Gene ID",required=False,default=None)
+    parser.add_argument('--add','-a', action="store_true",help="Add gene ID to the output",required=False)
     parser.add_argument('--build','-b', action="store",help="Genome build: default: 38",required=False,default="38")
+    parser.add_argument('--json','-j', action="store_true",help="Output JSON",required=False)
 
     try:
         args=parser.parse_args()
@@ -34,6 +36,8 @@ def main():
 
     build=args.build
     ID=args.id
+    add_id=args.add
+    out_json=args.json
 
     LOGGER=logging.getLogger("gene2uniprot")
     LOGGER.setLevel(verbosity)
@@ -58,8 +62,15 @@ def main():
     LOGGER.debug("\n%s\n" % json.dumps(xrefs,indent=4,sort_keys=True))
     uniprotID=xrefs["UniProtKB/Swiss-Prot"][0][0]
     df=uniprot.uniprot2df(uniprot.getUniprotData(uniprotID))
-    df["ID"]=ID
-    df.to_csv(sys.stdout,index=False,sep="\t",na_rep="NA",columns=["ID","Field","Value"])
+    out_columns=["Field","Value"]
+    if add_id:
+        df["ID"]=ID
+        out_columns=["ID","Field","Value"]    
+    if out_json:
+        df.fillna(value="NA",inplace=True)
+        df[out_columns].to_json(sys.stdout,orient="records")
+    else:
+        df.to_csv(sys.stdout,index=False,sep="\t",na_rep="NA",columns=out_columns)
 
 if __name__=="__main__":
     main()
