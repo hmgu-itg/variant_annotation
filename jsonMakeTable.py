46f2c1b705b3b622f06aeb1c154729abb34869ce
andrei <andrei@linux.fritz.box>
2023-07-12 20:30:37 +0000

    add command line options to change table appearance

--- jsonMakeTable.py
+++ jsonMakeTable.py
@@ -7,9 +7,14 @@ import json
 #----------------------------------------------------------------------------------------------------------------------------------
 
 def main():
-    parser=argparse.ArgumentParser(description="Create JSON table from STDIN")
+    parser=argparse.ArgumentParser(description="Encode JSON table")
+    parser.add_argument('--no-header','-n', action="store_true",help="Don't display table header",required=False,default=False)
+    parser.add_argument('--column-option','-o', action="store",help="Column option",required=False,default=None)
     parser.add_argument('--caption','-c', action="store",help="Caption",required=False,default=None)
     parser.add_argument('--title','-t', action="store",help="Section title",required=False,default=None)
+    parser.add_argument('--long','-l', action="store_true",help="Long table",required=False,default=False)
+    parser.add_argument('--landscape','-a', action="store_true",help="Landscape mode",required=False,default=False)
+    parser.add_argument('--newpage','-p', action="store_true",help="Trigger new page",required=False,default=False)
 
     try:
         args=parser.parse_args()
@@ -18,6 +23,11 @@ def main():
 
     caption=args.caption
     title=args.title
+    no_header=args.no_header
+    option=args.column_option
+    longtable=args.long
+    landscape=args.landscape
+    newpage=args.newpage
 
     if sys.stdin.isatty():
         parser.print_help()
@@ -32,6 +42,16 @@ def main():
        d["caption"]=caption
     if title:
        d["title"]=title
+    if no_header:
+        d["no_header"]=True
+    if landscape:
+        d["landscape"]=True
+    if longtable:
+        d["longtable"]=True
+    if newpage:
+        d["newpage"]=True
+    if option:
+        d["column_option"]=option
     print(json.dumps(d))
     
 if __name__=="__main__":
